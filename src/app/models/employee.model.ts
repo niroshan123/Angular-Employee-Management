62e0f620f327890def1142370a170711920ceab9
niroshan123 <Niroshan07@bitbucket.com>
2020-04-20 14:53:52 +0530

    Project-Setup-Employee-Class

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,15 @@
+export class Employee{
+
+    id:number;
+    name:string;
+    gender:string;
+    email?:string;//says its optional
+    phoneNumber?:number;
+    contactPreferences:string;
+    dateOfBirth:Date;
+    department:string;
+    isActive:boolean;
+    photo?:string;
+
+
+}
