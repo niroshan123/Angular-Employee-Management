32eb4ce85f4369eaa512707fb2c8fe58148e6180
niroshan123 <niroshanratnayake07@gmail.com>
2020-04-21 18:45:03 +0530

    angular create employee  page

--- src/app/employees/create-employee/create-employee.component.ts
+++ src/app/employees/create-employee/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee.model';
 import { NgForm } from '@angular/forms';
+import { Department } from 'src/app/models/department.model';
 
 @Component({
   // selector: 'app-create-employee',
@@ -8,13 +9,35 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+ 
+  departments: Department[]=[
+  {id:1,name:'IT'} , 
+  {id:2,name:'Engineering'} , 
+  {id:3,name:'Medicine'} , 
+  {id:4,name:'Science'} ,
+  {id:5,name:'Arts'}
+  
+  ];
+  gender:string;
+  email:string; //declare variable
+  fullName:string;
+  phoneNumber:number;
+  contactPref:string;
+  isActive:boolean;
+  department:string;
+  
+  
 
-  constructor() { }
+
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
 
-  saveEmployee(empForm:NgForm):void{
+saveEmployee(empForm:NgForm):void{
 console.log(empForm.value);
   }
 
